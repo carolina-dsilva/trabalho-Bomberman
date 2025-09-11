a7084f582e0681536d7e1d8ca8aadbb1a1253f55
carolina-dsilva <carolina.dsillva@gmail.com>
2025-09-11 19:05:31 +0000

    03

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
-/**
-    Estrutura inicial para um jogo
-    versão: 0.1 (Prof. Alex,  Adaptado Prof. Felski)
-*/
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void explosaoBomba(int m[15][45], int xBomba, int yBomba, int *contBomba){
@@ -49,9 +47,9 @@ int main()
                     1,0,0,0,2,2,0,2,2,0,2,0,2,2,0,2,2,0,2,2,0,2,1,0,2,2,0,2,2,0,2,2,0,2,2,0,2,2,0,2,2,0,2,2,1,
                     1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,
                     1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,2,1,
-                    1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1,2,1,
-                    1,0,0,2,2,0,0,0,0,2,1,0,0,2,0,0,0,2,2,0,0,0,0,0,0,1,0,0,0,0,2,2,2,0,1,0,0,0,0,2,0,1,0,0,1,
-                    1,2,1,2,1,1,1,2,1,0,1,0,1,2,1,1,1,2,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,2,1,0,1,2,1,
+                    1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,0,0,2,1,
+                    1,0,0,2,2,0,0,0,0,2,1,0,0,2,0,0,0,2,2,0,0,0,0,0,0,1,0,0,0,0,2,2,2,0,1,0,0,0,0,2,0,0,1,0,1,
+                    1,2,1,2,1,1,1,2,1,0,1,0,1,2,1,1,1,2,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,2,1,0,0,2,1,
                     1,2,1,0,0,0,2,2,0,0,1,1,1,0,0,0,1,0,0,0,0,2,0,0,0,0,2,2,0,0,0,0,1,0,0,2,0,0,0,0,1,0,0,2,1,
                     1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,2,1,1,1,0,1,2,1,1,1,0,1,0,1,2,1,2,1,0,1,0,1,0,1,
                     1,2,0,2,0,0,0,0,0,0,1,0,0,0,0,2,2,0,1,2,2,0,0,0,0,1,0,0,0,0,0,0,0,2,2,0,2,2,0,0,0,1,0,2,1,
@@ -63,56 +61,102 @@ int main()
 
     int x=3, y=1, xBomba, yBomba;/// Posição inicial do personagem
     int contBomba = 0;/// Contador da bomba
+    int xInimigo[3] = {10, 4, 3};
+    int yInimigo[3] = {10, 20, 42};
+    unsigned long tempoAtual = GetTickCount();
+    unsigned long tempoUltimoMovimentoInimigos = 0;
+    const unsigned long intervaloMovimentoInimigos = 1000;
     unsigned long tempoBomba = 0;
-    char tecla;/// Variavel para tecla precionada
+    char tecla;
 
     while(true){
-        ///Posiciona a escrita no iicio do console
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        tempoAtual = GetTickCount();
 
-        ///Imprime o jogo
+        // movimentação dos inimigos
+        if (tempoAtual - tempoUltimoMovimentoInimigos >= intervaloMovimentoInimigos) {
+            for(int k = 0; k < 3; k++){
+                int direcao = rand() % 4;
+                int novoX = xInimigo[k];
+                int novoY = yInimigo[k];
+
+                for (int passos = 0; passos < 3; passos++) {
+                    int proximoX = novoX;
+                    int proximoY = novoY;
+
+                    switch(direcao){
+                        case 0: proximoX--; break;
+                        case 1: proximoX++; break;
+                        case 2: proximoY--; break;
+                        case 3: proximoY++; break;
+                    }
+
+                    if (proximoX >= 0 && proximoX < 15 && proximoY >= 0 && proximoY < 45 && m[proximoX][proximoY] == 0) {
+                        novoX = proximoX;
+                        novoY = proximoY;
+                    } else {
+                        break;
+                    }
+                }
+
+                xInimigo[k] = novoX;
+                yInimigo[k] = novoY;
+            }
+            tempoUltimoMovimentoInimigos = tempoAtual;
+        }
+
+        /// Imprime o mapa
         for(int i=0;i<15;i++){
             for(int j=0;j<45;j++){
-                if(i==x && j==y){
-                    cout << char(36); //personagem
-                } else {
-                    switch (m[i][j]){
-                        case 0: cout << " "; break; //caminho
-                        case 1: cout << char(219); break; //parede fixa
-                        case 2: cout << char(176);break; //parede que quebra
-                        case 3: cout << char(42);break; // Bomba
-                        case 4: cout << char(64);break;//explosão da bomba
+                bool desenhouInimigo = false;
+                for (int k = 0; k < 3; k++) {
+                    if (xInimigo[k] != -1 && yInimigo[k] != -1 && i == xInimigo[k] && j == yInimigo[k]) {
+                        cout << char(60); // inimigo
+                        desenhouInimigo = true;
+                        break;
+                    }
+                }
+                if(!desenhouInimigo){
+                    if(i==x && j==y){
+                        cout << char(36);
+                    } else {
+                        switch (m[i][j]){
+                            case 0: cout << " "; break; //caminho
+                            case 1: cout << char(219); break; //parede fixa
+                            case 2: cout << char(176);break; //parede que quebra
+                            case 3: cout << char(42);break; // Bomba
+                            case 4: cout << char(64);break;//explosão da bomba
+                        }
                     }
                 }
             }
             cout<<"\n";
         }
 
-        ///executa os movimentos
+        ///movimentos
         if ( _kbhit() ){
             tecla = getch();
-            switch(tecla)
-            {
-                case 72: case 'w': ///cima
+            switch(tecla){
+                case 72: case 'w':
                     if(m[x-1][y] == 0 || m[x-1][y] == 4){
                         x--;
                     }
-                break;
-                case 80: case 's': ///baixo
-                    if(m[x+1][y] == 0 || m[x-1][y] == 4){
-                        x++;
+                    break;
+                case 80: case 's':
+                    if(m[x+1][y] == 0 || m[x+1][y] == 4){
+                       x++;
                     }
-                break;
-                case 75:case 'a': ///esquerda
-                    if(m[x][y-1] == 0 || m[x-1][y] == 4){
+                    break;
+                case 75: case 'a':
+                    if(m[x][y-1] == 0 || m[x][y-1] == 4){
                         y--;
                     }
-                break;
-                case 77: case 'd': ///direita
-                    if(m[x][y+1] == 0 || m[x-1][y] == 4){
+                    break;
+                case 77: case 'd':
+                    if(m[x][y+1] == 0 || m[x][y+1] == 4){
                         y++;
                     }
-                break;
+                    break;
                 case 32:
                     if(contBomba == 0){
                         m[x][y] = 3;
@@ -121,14 +165,16 @@ int main()
                         yBomba = y;
                         tempoBomba = GetTickCount();
                     }
-                break;
+                    break;
             }
         }
-        if(contBomba == 1 && GetTickCount() - tempoBomba >= 3000){
+
+        if(contBomba == 1 && GetTickCount() - tempoBomba >= 1500){
             explosaoBomba(m, xBomba, yBomba, &contBomba);
             tempoBomba = GetTickCount();
         }
-        if(contBomba == 2 && GetTickCount() - tempoBomba >= 3000){
+
+        if(contBomba == 2 && GetTickCount() - tempoBomba >= 1700){
             for(int i=0;i<15;i++){
                 for(int j=0;j<45;j++){
                     if(m[i][j] == 4){
@@ -138,7 +184,76 @@ int main()
             }
             contBomba = 0;
         }
-    } //fim do laco do jogo
 
+        for (int k = 0; k < 3; k++) {
+            if (xInimigo[k] != -1 && yInimigo[k] != -1) { // só checa inimigos vivos
+                if (m[xInimigo[k]][yInimigo[k]] == 4) {
+                    xInimigo[k] = -1;
+                    yInimigo[k] = -1;
+                }
+            }
+        }
+
+        bool encostouInimigo = false;
+        for (int k = 0; k < 3; k++) {
+            if (xInimigo[k] != -1 && yInimigo[k] != -1) { // só inimigos vivos
+                if (x == xInimigo[k] && y == yInimigo[k]) {
+                    encostouInimigo = true;
+                    break;
+                }
+            }
+        }
+        //checa se todos inimigos morreram)
+        bool venceu = true;
+        for (int k = 0; k < 3; k++) {
+            if (xInimigo[k] != -1 && yInimigo[k] != -1) {
+                venceu = false;
+                break;
+            }
+        }
+
+        if (venceu) {
+            for(int i = 0; i < 15; i++){
+                for(int j = 0; j < 45; j++){
+                    m[i][j] = 0;
+                }
+            }
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+            for(int i = 0; i < 15; i++){
+                for(int j = 0; j < 45; j++){
+                    cout << " ";
+                }
+                cout << "\n";
+            }
+            COORD topo;
+            topo.X = 15;
+            topo.Y = 2;
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), topo);
+            cout << "\n\nVOCE VENCEU!\n";
+            Sleep(3000);
+            break;
+        }
+        if (m[x][y] == 4 || encostouInimigo) {
+            for(int i = 0; i < 15; i++){
+                for(int j = 0; j < 45; j++){
+                    m[i][j] = 0;
+                }
+            }
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+            for(int i = 0; i < 15; i++){
+                for(int j = 0; j < 45; j++){
+                    cout << " ";
+                }
+                cout << "\n";
+            }
+            COORD topo;
+            topo.X = 15;
+            topo.Y = 2;
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), topo);
+            cout << "VOCE MORREU!";
+            Sleep(3000);
+            break;
+        }
+    }
     return 0;
-} //fim main
+}
